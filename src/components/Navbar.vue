3ccfe3c23fb1aa40eb58538b9d4d50710652e3f2
김희주 <ddkkmd@naver.com>
2020-08-10 23:32:50 +0900

    add: 장바구니,로그인,마이페이지,알림 뱃지 추가

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,18 +3,49 @@
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
         <v-toolbar dark color="teal accent-4" flat>
             <v-toolbar-title>Commerce</v-toolbar-title>
+            <!--             :search-input.sync="search" == v-model         -->
             <v-autocomplete
                     :search-input.sync="search"
                     class="mx-4"
                     flat
                     hide-no-data
                     hide-details
-                    @keyup.enter="onChange"
+                    @keyup.enter="searchByKeyword"
                     label="검색어를 입력하세요."
                     solo-inverted
             ></v-autocomplete>
         </v-toolbar>
         <v-spacer></v-spacer>
+
+        <a href="/mypage" v-if="account">
+            <v-icon icon color="grey lighten-2" large>
+                mdi-account-circle
+            </v-icon>
+        </a>
+        <a href="/logout" v-if="account">
+            <v-icon icon color="grey lighten-2" large>
+                mdi-lock-open-variant
+            </v-icon>
+        </a>
+
+        <a href="/login" v-if="!account">
+            <v-icon icon color="grey lighten-2" large>
+                mdi-lock
+            </v-icon>
+        </a>
+
+        <div class="text-xs-center" v-if="account">
+<!--            <v-badge left>-->
+<!--                <span slot="badge">6</span>-->
+                <v-icon large color="grey lighten-2">mdi-cart-outline</v-icon>
+<!--            </v-badge>-->
+
+<!--            <v-badge color="red">-->
+<!--                <span slot="badge">!</span>-->
+                <v-icon large color="grey lighten-2">mdi-bell-ring-outline</v-icon>
+<!--            </v-badge>-->
+        </div>
+
     </v-app-bar>
 </template>
 
@@ -23,12 +54,14 @@
         name: "Navbar",
         data() {
             return {
-                search: null
+                search: null,
+                account: null
             }
         },
         methods: {
-            onChange() {
-               console.log(this.search)
+            searchByKeyword() {
+                // todo axios
+                console.log(this.search);
             }
         }
     }
