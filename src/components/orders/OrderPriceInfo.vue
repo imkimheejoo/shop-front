1b7ff0c05d6717b4609c729c706c8ad7b0cbad54
imkimheejoo <jujudamdadi@naver.com>
2020-09-14 08:32:07 +0900

    feat: 주문페이지 컴포넌트 레이아웃 구성

---
+++ src/components/orders/OrderPriceInfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>order price info</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>sss</div>
+  <v-container class="mt-5">
+    <order-products></order-products>
+    <cupons></cupons>
+    <delivery></delivery>
+    <order-price-info></order-price-info>
+    <v-btn block color="secondary" dark style="height:45px; font-size:18px">결제하기</v-btn>
+  </v-container>
 </template>
 
 <script>
-export default {};
+import OrderProducts from "../components/orders/OrderProducts.vue";
+import Cupons from "../components/cupons/Cupons";
+import Delivery from "../components/deliveries/Delivery";
+import OrderPriceInfo from "../components/orders/OrderPriceInfo";
+
+export default {
+  components: {
+    OrderProducts,
+    Cupons,
+    Delivery,
+    OrderPriceInfo,
+  },
+};
 </script>
 
 <style>
